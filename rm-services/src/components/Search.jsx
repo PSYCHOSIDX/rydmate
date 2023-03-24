0222821baa413df7e4df9abc3a68f3836c97a104
Siddhesh Kamath <siddheshkamath98@gmail.com>
2023-03-24 23:05:19 +0530

    ..

--- rm-services/src/components/Search.jsx
+++ rm-services/src/components/Search.jsx
@@ -20,13 +20,13 @@ const Search = () => {
                   <button  className='go-btn'> Go </button>
                 </div>
                 <div className="search-card">
-                  <h1>Search <br/> Rides</h1>
-                  <h4>Find rides quick at best  prices</h4>
+                  <h1>Search <br/> Signup / <br/> Login</h1>
+                  <h4>register / login to access services</h4>
                   <button  className='go-btn'> Go </button>
                 </div>
                 <div className="search-card">
-                  <h1>Search <br/> Rides</h1>
-                  <h4>Find rides quick at best  prices</h4>
+                  <h1>Search <br/> Register In <br/>Rider Program</h1>
+                  <h4>Join our program to offer rides</h4>
                   <button className='go-btn'> Go </button>
                 </div>
 
