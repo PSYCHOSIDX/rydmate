3238197a713745ffcdf21c4e8cf4976181d89a9c
Siddhesh Kamath <siddheshkamath98@gmail.com>
2023-05-11 01:37:00 +0530

    Update index.js

--- rm-services/src/index.js
+++ rm-services/src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 
 import React from 'react';
 
-import {BrowserRouter } from "react-router-dom"
+import {BrowserRouter as Router } from "react-router-dom"
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -10,8 +10,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
 
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>
+  </Router>
 
 );
