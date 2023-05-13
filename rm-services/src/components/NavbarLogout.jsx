e1770da6706667ac3d78101ae7bea03e5f9f583b
Siddhesh Kamath <siddheshkamath98@gmail.com>
2023-05-13 18:43:50 +0530

    ..

--- rm-services/src/components/NavbarLogout.jsx
+++ rm-services/src/components/NavbarLogout.jsx
@@ -1,3 +1,4 @@
+
 import React , {useEffect}from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -10,12 +11,38 @@ import { UserAuth } from '../context/UserAuthContext';
 import { useState } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import {FaRegUser} from 'react-icons/fa'
-import { collection, query, where ,onSnapshot} from "firebase/firestore";
+// eslint-disable-next-line
+import { collection, query, where ,onSnapshot, doc, updateDoc, collectionGroup, } from "firebase/firestore";
 import { db } from '../firebaseConfig';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 function StaticExample() {
+  const {user} = UserAuth();
+  
+  const [phoneNoPara, setPhoneNo]= useState("");
+  const [Age, setAge] = useState("");
+  const [sex, setSex]= useState("");
+  const userId = user.uid;
+
+  const handleUpdate = async (e) => {
+    e.preventDefault()
+    const DocRef = doc(db, 'users', userId)
+    try{
+      await updateDoc(DocRef, {
+        age: Age,
+        phoneNo: phoneNoPara,
+        gender:sex
+      })
+      alert('Your profile is ready')
+    } catch (err) {
+      alert(err)
+    }    
+  }
+
+
+
   return (
     <div
       className="modal show"
@@ -27,20 +54,33 @@ function StaticExample() {
         </Modal.Header>
 
         <Modal.Body>
-          <p id='modal-text'></p>
+        <Form xs="auto" className="sign-form" onSubmit={handleUpdate}>
+          <Form.Group className="mb-3 none" controlId="formBasicEmail">
+                <Form.Label>Age</Form.Label>
+                <Form.Control onChange={(e) => setAge(e.target.value)} type="number" placeholder="Enter Age" required/>
+              </Form.Group>
+              <Form.Group className="mb-3 none" controlId="formBasicPassword">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control onChange={(e) => setPhoneNo(e.target.value)} type="tel" name="phoneNumber" id="phoneNumber"  pattern="[0-9]{10}" placeholder="Enter Phone Number " required />
+              </Form.Group>
+              <Form.Group className="mb-3 none" controlId="formBasicEmail">
+                <Form.Label>Gender</Form.Label>
+                <Form.Control onChange={(e) => setSex(e.target.value)} type="text" placeholder="Enter Gender" required/>
+              </Form.Group>
+
+              <Button variant="primary" type='submit'>Save changes</Button>
+
+    </Form>
         </Modal.Body>
 
-        <Modal.Footer>
-          <Button variant="secondary">Close</Button>
-          <Button variant="primary">Save changes</Button>
-        </Modal.Footer>
+
       </Modal.Dialog>
     </div>
   );
 }
 
 
-
+// This is profile finction this contains all sidebar data
 
 
 function Profile() {
@@ -49,34 +89,47 @@ function Profile() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const [loading, setLoading] = useState(false);
+
   const {user} = UserAuth();
   const [fireUser, setFireUser]= useState([]);
   
-  const colletionRef = collection(db, 'users');
-  const userEmail = user.email;
+  
   useEffect(() => {
-    const q = query(
-      colletionRef,
-      where('email', '==', userEmail) 
-    );
+    const userEmail = user.email;
+    const q = query(collection(db, 'users'), where('email', '==', userEmail))
+    onSnapshot(q, (querySnapshot) => {
+      setFireUser(querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data()
+      })))
+    })
+    
+  },[])
 
-    setLoading(true);
-    // const unsub = onSnapshot(q, (querySnapshot) => {
-    const unsub = onSnapshot(q, (querySnapshot) => {
-      const items = [];
-      querySnapshot.forEach((doc) => {
-        items.push(doc.data());
-      });
-      setFireUser(items);
-      setLoading(false);
-    });
-    return () => {
-      unsub();
-    };
 
-    // eslint-disable-next-line
-  }, []);
+
+  // useEffect(() => {
+  //   const colletionRef = collection(db, 'users');
+  //   const userEmail = user.email;
+    
+  //   const q = query(colletionRef,where('email', '==', true && userEmail) );
+
+  //   setLoading(true);
+    
+  //   const unsub = onSnapshot(q, (querySnapshot) => {
+  //     const items = [];
+  //     querySnapshot.forEach((doc) => {
+  //       items.push(doc.data());
+  //     });
+  //     setFireUser(items);
+  //     setLoading(false);
+  //   });
+  //   return () => {
+  //     unsub();
+  //   };
+
+  //   // eslint-disable-next-line
+  // }, []);
  
   return (
     
@@ -91,19 +144,30 @@ function Profile() {
         </Offcanvas.Header>
     <br/>
     
-        {loading ? <h1>Loading...</h1> : null}
         {fireUser.map((fire) => (
           
         <Offcanvas.Body>
       
-       {fire.photoURL ? <img src={user.photoURL} alt='' className='profile'/> : <FaRegUser  className='icon'/>}
+       {fire.data.photoURL ? <img src={user.photoURL} alt='' className='profile'/> : <FaRegUser  className='icon'/>}
           <h4> <b>Name</b> </h4>
-          <h5>{fire.displayName}</h5> 
+          <h5>{fire.data.displayName}</h5> 
           <br/>
           <h4><b>Email</b></h4>
-          <h5>{fire.email}</h5>
-         
-          {fire.phoneNo ? <p>found</p>: <StaticExample/>}
+          <h5>{fire.data.email}</h5>
+     
+          {fire.data.phoneNo ? 
+          <div>
+          <h4> <b>Phone Number</b> </h4>
+          <h5>{fire.data.phoneNo}</h5> 
+          <br/>
+          <h4><b>Age</b></h4>
+          <h5>{fire.data.age}</h5>
+          <br/>
+          <h4><b>Gender</b></h4>
+          <h5>{fire.data.gender}</h5>
+          </div>
+          
+          : <StaticExample/>}
         </Offcanvas.Body>
 
     
@@ -136,7 +200,7 @@ function InfoPage() {
 function NavbarLogout(){
   const {logout} = UserAuth();
   const navigate = useNavigate();
-  
+ 
   const handleLogout = async () => {
     try{
       await logout();
@@ -160,7 +224,7 @@ function NavbarLogout(){
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto nav-hold">
            
-            <InfoPage/>
+           <InfoPage/>
 
             <Link to="" className='link'>
             <button onClick={handleLogout} className='btn-contact'> Logout </button>
