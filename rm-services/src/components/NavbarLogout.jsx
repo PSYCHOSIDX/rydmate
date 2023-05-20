b3fb1fe65a29e4024c313e926e563c078581798b
Siddhesh Kamath <63893110+PSYCHOSIDX@users.noreply.github.com>
2023-05-20 21:09:04 +0530

    Update NavbarLogout.jsx

--- rm-services/src/components/NavbarLogout.jsx
+++ rm-services/src/components/NavbarLogout.jsx
@@ -133,10 +133,8 @@ function Profile() {
 
           <Link className='link' to='/emergency'>
 
-          <Button variant="success" id='update-button'
- type="submit" value="Emergency Contacts"> 
-Emergency Contacts 
-<Button/>
+          <Button href='' id='update-button'
+ type="submit" value="Emergency Contacts"/> 
           </Link>
           
           <button onClick={handleLogout} className='btn-contact' id='visible'> Logout </button>
