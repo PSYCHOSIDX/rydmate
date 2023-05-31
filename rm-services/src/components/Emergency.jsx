85ab25ff011e70facad45bdc2cebbceaaa190d6e
Siddhesh Kamath <siddheshkamath98@gmail.com>
2023-06-01 00:50:40 +0530

    ..

--- rm-services/src/components/Emergency.jsx
+++ rm-services/src/components/Emergency.jsx
@@ -53,7 +53,7 @@ const workTwilio = async (e) => {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ to: '+917028193277','+919011672956' , body: ' RydMate Emergency Alert \n'+ user.displayName +' with email id '+user.email+'\nNeeds Your Help , please inform your nearest police station\nUsers Last Live Co-ordinates are : \n Latitude : '+lat+'\n Longitude : ' +long }),
+    body: JSON.stringify({ to: ['+917028193277','+919011672956'] , body: ' RydMate Emergency Alert \n'+ user.displayName +' with email id '+user.email+'\nNeeds Your Help , please inform your nearest police station\nUsers Last Live Co-ordinates are : \n Latitude : '+lat+'\n Longitude : ' +long }),
    
   }
    );
