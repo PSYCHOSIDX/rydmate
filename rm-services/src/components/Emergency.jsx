8d8f40ce15fb1b663198f2dc91999ac35e1a6253
Siddhesh Kamath <63893110+PSYCHOSIDX@users.noreply.github.com>
2023-05-31 20:50:17 +0530

    Update Emergency.jsx

--- rm-services/src/components/Emergency.jsx
+++ rm-services/src/components/Emergency.jsx
@@ -42,7 +42,7 @@ const onSubmit = async (e) => {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ to: emegerncyList , body: 'RydMate Emergency Alert'+user.displayName +'Needs Your Help ' }),
+    body: JSON.stringify({ to: {emegerncyList}, body: 'RydMate Emergency Alert'+user.displayName +'Needs Your Help ' }),
   });
 
   const data = await res.json();
