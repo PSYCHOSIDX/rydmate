6c6e77a05f0c58224f0a5ef7a95ecfe5b8bb7af8
Siddhesh Kamath <siddheshkamath98@gmail.com>
2023-06-01 22:43:41 +0530

    ..

--- rm-services/src/components/Search.jsx
+++ rm-services/src/components/Search.jsx
@@ -35,7 +35,7 @@ const Search = () => {
                 <div className="search-card">
                   <h1> Register In <br/>Rider Program</h1>
                   <h4>Join our program to offer rides</h4>
-                  <Link className='link' to='/riderprogram'>
+                  <Link className='link' to='/join'>
                   <button className='go-btn'> Go </button>
                   </Link>
                 </div>
