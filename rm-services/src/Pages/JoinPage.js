47855bfd37adb56ebdf3f69fc54a82393f5b581b
Siddhesh Kamath <63893110+PSYCHOSIDX@users.noreply.github.com>
2023-06-11 21:22:11 +0530

    Update JoinPage.js

--- rm-services/src/Pages/JoinPage.js
+++ rm-services/src/Pages/JoinPage.js
@@ -114,7 +114,7 @@ console.log(distance)
     var options = {
       key: process.env.REACT_APP_RAYZORPAY_KEY_ID ,
       key_secret: process.env.REACT_APP_RAYZORPAY_KEY_SECRET ,
-      amount: finalCost*1000 ,
+      amount: finalCost*100 ,
       currency:"INR",
       name:"RydMate",
       receipt:'receipt'+shortid.generate() ,
